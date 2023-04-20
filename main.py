9e012096962d01f964027140605b420020774399
jackzwumbo <fiandry53@gmail.com>
2023-04-20 21:51:55 +0700

    Making Sequential Explanation done

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Semua sintaksis dasar bahasa pemograman terdiri dari:
+1. Sekuensial: Langkah berurutan
+2. Percabangan: Langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulang Langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+# Sekuensial
+print('Mom ask, "Please go to the shop"')
+print('Budi answer, "Ok, what must i do at the shop?"')
+print('Mom answer, "buy one bottle of milk, and if there\'s an egg buy 6"')
+print("so budi go to the shop")
+print("and start shopping")
