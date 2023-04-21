f505e55d9c4cc072c3f7a299b66443d9b182b8e9
jackzwumbo <fiandry53@gmail.com>
2023-04-21 22:22:54 +0700

    Trying to code meme story about mom ask son to buy milk and egg

--- Meme story, Son buy milk and egg.py
+++ Meme story, Son buy milk and egg.py
@@ -7,10 +7,10 @@ print("Son go to the shop")
 
 milk_available = input("is the milk available? (yes/no)")
 
-if milk_available=="yes" :
+if milk_available == "yes":
     print("Son is checking the price, enough")
     egg_available = input("is the egg available? (yes/no)")
-    if egg_available=="yes" :
+    if egg_available == "yes":
         print("Son is checking the egg stock qty in the shop, enough stock")
         print("son buy one bottle of milk and 6 egg")
     else:
@@ -21,4 +21,3 @@ else:
 print("son go home")
 print("son give the result to mom")
 print("finish")
-
